f14f8873da8b2b54bbd80ddb45a62f7ca1354f5a
Sebastian Gutierrez <sebastiangutierrezcorrea@gmail.com>
2019-04-10 11:03:20 -0500

    Rename styles folder to scss

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta http-equiv="x-ua-compatible" content="ie=edge">
         <title>Sebastian Gutierrez</title>
-        <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
         <link href="https://fonts.googleapis.com/css?family=IBM+Plex+Mono:400,400i,700,700i|IBM+Plex+Sans:400,400i,700,700i|Volkhov:400,400i,700,700i" rel="stylesheet">
         <link rel="stylesheet" href="{{ mix('css/main.css', 'assets/build') }}">
     </head>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ mix.webpackConfig({
 });
 
 mix.js('source/_assets/js/main.js', 'js')
-    .sass('source/_assets/sass/main.scss', 'css')
+    .sass('source/_assets/scss/main.scss', 'css')
     .options({
         processCssUrls: false,
     }).version();
