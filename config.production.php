473f9eca1a252e0134363146918dc616d741b69f
Sebastian Gutierrez <sebastian@rocketfy.co>
2021-12-26 22:40:01 -0500

    delete publised filter to use Netlify CMS workflow

--- config.production.php
+++ config.production.php
@@ -3,11 +3,4 @@
 return [
   'production' => true,
   'baseUrl' => 'sebastiangutierrez.co/',
-  'collections' => [
-    'posts' => [
-        'filter' => function ($item) {
-            return $item->published;
-        }
-    ],
-  ],
 ];
